8b32fe13c01a44bcc6be0901ce22e4c34560d8f2
Adam Andrews <andrewsadam07@gmail.com>
2022-08-15 14:44:10 +0100

    Added Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,75 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import {
+	MenuIcon,
+	ChevronDownIcon,
+	HomeIcon,
+	SearchIcon,
+} from '@heroicons/react/solid';
+import {
+	BellIcon,
+	ChatIcon,
+	GlobeIcon,
+	PlusIcon,
+	SparklesIcon,
+	SpeakerphoneIcon,
+	VideoCameraIcon,
+} from '@heroicons/react/outline';
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+function Header() {
+	return (
+		<div className="flex sticky top-0 z-50 bg-white px-4 py-2 shadow-sm">
+			<div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+				<Image
+					src="https://links.papareact.com/fqy"
+					objectFit="contain"
+					layout="fill"
+				/>
+			</div>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+			<div className="flex items-center mx-7 xl:min-w-[300px]">
+				<HomeIcon className="h-5 w-5" />
+				<p className="flex-1 hidden ml-2 lg:inline">Home</p>
+				<ChevronDownIcon className="h-5 w-5" />
+			</div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+			<form className="flex flex-1 items-center space-x-2 border border-gray-200 rounded-sm bg-gray-100 px-3 py-1">
+				<SearchIcon className="h-6 w-6 text-gray-400" />
+				<input
+					className="flex-1 bg-transparent"
+					type="text"
+					placeholder="Search Reddit"
+				/>
+				<button type="submit" hidden></button>
+			</form>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+			<div className="items-center space-x-2 text-gray-500 hidden lg:inline-flex">
+				<BellIcon className="icon" />
+				<ChatIcon className="icon" />
+				<GlobeIcon className="icon" />
+				<PlusIcon className="icon" />
+				<SparklesIcon className="icon" />
+				<SpeakerphoneIcon className="icon" />
+				<VideoCameraIcon className="icon" />
+			</div>
+			<div className="inline-flex lg:hidden items-center ml-5">
+				<MenuIcon className="icon" />
+			</div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+			{/* {Sign in Sign Out} */}
+			<div className="hidden items-center lg:flex space-x-2 border border-gray-100 p-2 cursor-pointer">
+				<div className="relative h-5 w-5 flex-shrink-0 cursor-pointer">
+					<Image
+						objectFit='contain'
+						src="https://links.papareact.com/23l"
+						layout="fill"
+						alt=""
+					/>
+				</div>
+				<p className="text-gray-400"> Sign In</p>
+			</div>
+		</div>
+	);
 }
 
-export default Home
+export default Header;
