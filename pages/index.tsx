3b61a38145c5798f74593eb89af6482ee256f9d4
Adam Andrews <andrewsadam07@gmail.com>
2022-08-15 15:01:39 +0100

    Added postbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Avatar from '../components/Avatar'
+import PostBox from '../components/Postbox'
 const Home: NextPage = () => {
 
   return (
@@ -9,7 +10,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit 2.0 Clone</title>
         </Head>
-        <Avatar/>
+        <PostBox/>
     </div>
   )
 }
